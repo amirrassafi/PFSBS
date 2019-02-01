fc9c711454a46676418ff60a340c0c7e093c3f89
amir rassafi <amir.rassafi@gmail.com>
2019-02-01 23:20:59 +0330

    Acc added

---
+++ accuracy.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors as knn
+from sklearn.model_selection import cross_val_score
+
+alpha = 0.01
+
+def train_acc(x, trn):
+    if sum(x) == 0 : return np.inf
+
+    pass
+
+def test_acc():
+    pass
