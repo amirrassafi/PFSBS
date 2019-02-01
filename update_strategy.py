ff37d2a51dd2a7978ec3e016a688b74587a372cf
amir rassafi <amir.rassafi@gmail.com>
2019-02-01 18:38:09 +0330

    Some bug fixed in drawing and functions

--- update_strategy.py
+++ update_strategy.py
@@ -18,10 +18,10 @@ UPDATE_STRATEGIES ={
             },
     "TCSSA3":
             {
-                "S1":(lambda t, T: 1.95 - 2.0*t**(1.0/3)*T**(1.0/3)),
-                "S2":(lambda t, T: 1.95 - 2.0*t**(1.0/3)*T**(1.0/3)),
-                "S3":(lambda t, T: -2.0 * t**3/T**3 + 2.5),
-                "S4":(lambda t, T: -2.0 * t**3/T**3 + 2.5)  
+                "S1":(lambda t, T: 1.95 - 2.0*t**(1.0/3)/T**(1.0/3)),
+                "S2":(lambda t, T: 1.95 - 2.0*t**(1.0/3)/T**(1.0/3)),
+                "S3":(lambda t, T: (-2.0 * t**3/T**3) + 2.5),
+                "S4":(lambda t, T: (-2.0 * t**3/T**3) + 2.5)  
             }
 }
 
@@ -29,12 +29,12 @@ if __name__ == '__main__':
     T = 200
     fig = plt.figure()
     l = 1
-    for st in UPDATE_STRATEGIES:
-        for sb in UPDATE_STRATEGIES[st]:
+    for st, c in zip(UPDATE_STRATEGIES, ['r', 'g', 'b']):
+        for sb in sorted(UPDATE_STRATEGIES[st]):
             f = UPDATE_STRATEGIES[st][sb]
             s = [f(t, T) for t in range(T)]
             ax = fig.add_subplot(3, 4, l)
             l = l+1
-            ax.plot(s, label=st+"-"+sb)
+            ax.plot(s, c, label=st+"-"+sb)
             ax.legend()
     plt.show()
