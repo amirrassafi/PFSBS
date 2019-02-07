3d1667462b8560beb4e78de081aca5e5b7ed0d52
amir rassafi <amir.rassafi@gmail.com>
2019-02-07 15:57:23 +0330

    Cost name changed

--- bssa.py
+++ bssa.py
@@ -1,9 +1,8 @@
 from salp import Salp
-from accuracy import train_acc
+from accuracy import cal_cost
 import numpy as np
 class BSSA:
-
-
+    
     def __init__(self, pop, dim, tf, ub, lb, upate_strategy):
         self.__pop = [Salp(dim, ub, lb) for i in range(pop)]
         self.__tf = tf
@@ -12,15 +11,17 @@ class BSSA:
         self.__food_history = []
         self.__cost_history = []
         self.__update_strategy = upate_strategy
+        self.__cost_func = cal_cost
 
     def train(self, max_iteration, train_data, train_target, test_data, test_target):
         self.__food_history = []
         self.__cost_history = []
         us = self.__update_strategy
         tf = self.__tf
+        cf = self.__cost_func
         #measure accuracy and sort respect to the fitness
         for s in self.__pop:
-            c = train_acc(s.get_position(), train_data, train_target)
+            c = cf(s.get_position(), train_data, train_target)
             s.set_cost(c)
 
         self.__pop = sorted(self.__pop, key=lambda x:x.get_cost())
@@ -32,7 +33,7 @@ class BSSA:
             for this_s, pre_s, j in zip(self.__pop, 
                                         self.__pop[-1:] + self.__pop[:-1], 
                                         range(len(self.__pop))):
-                if j<= len(self.__pop)/2:
+                if j<= len(self.__pop)/2.0:
                     c2 = np.random.rand(this_s.get_dim())
                     c3 = np.random.rand(this_s.get_dim())
                     c3 = np.array(list(map(lambda x:-1 if x<0.5 else 1, c3)))
@@ -46,11 +47,10 @@ class BSSA:
                 pos = s.get_position()
                 pos = np.array(list(map(lambda x:l if x<l else(u if x>u else x), pos)))
                 s.set_position(pos)
-                c = train_acc(s.get_position(), train_data, train_target)
+                c = cf(s.get_position(), train_data, train_target)
                 if c < food.get_cost():
                     food = s
             self.__food_history.append(food)      
             self.__cost_history.append(food.get_cost())
-            print("iteratation {}".format(i))
-        print(self.__cost_history)
+            print("iteratation {} cost {}".format(i, food.get_cost()))
         return self.__cost_history, self.__food_history
