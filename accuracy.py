6991b1a23ed1ff990a2ba02e2f14f551d7a282da
amir rassafi <amir.rassafi@gmail.com>
2019-02-07 10:51:47 +0330

    Train and Test accuracy added and test passed

--- accuracy.py
+++ accuracy.py
@@ -1,13 +1,39 @@
 import numpy as np
-from sklearn.neighbors import NearestNeighbors as knn
-from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier as knn
+from sklearn.model_selection import cross_val_score as cv
+from sklearn.metrics import accuracy_score as ac_sc
 
 alpha = 0.01
 
-def train_acc(x, trn):
+def train_acc(x, trn, trg):
+    x = [int(a) for a in np.round(x)]
+    x = [i for i in range(len(x)) if x[i]==1]
     if sum(x) == 0 : return np.inf
+    trn = trn.reshape(trn.shape[1], -1)
+    trn = trn[x, :]
+    trn = np.transpose(trn)
+    clf = knn(n_neighbors=5)
+    clf.fit(trn, trg)
+    score = cv(clf, trn, trg, cv=5, scoring="accuracy")
+    score = np.average(score)
+    return (1-alpha)*score + alpha * (sum(x)/len(x))
+    
 
-    pass
+def test_acc(x, tst, tst_trg, trn, trn_trg):
+    x = [int(a) for a in np.round(x)]
+    x = [i for i in range(len(x)) if x[i]==1]
+    tst = tst.reshape(tst.shape[1], -1)
+    tst = tst[x, :]
+    tst = np.transpose(tst)
+    trn = trn.reshape(trn.shape[1], -1)
+    trn = trn[x, :]
+    trn = np.transpose(trn)
+    clf = knn(n_neighbors=5)
+    clf.fit(trn, trn_trg)
+    tst_pred = clf.predict(tst)
+    acc = ac_sc(tst_trg, tst_pred)
+    print(acc)
+    return acc
 
-def test_acc():
-    pass
+
+    
