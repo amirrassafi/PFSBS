bdc7414a7610e95234673fb12dc56c7d1cc8532b
amir rassafi <amir.rassafi@gmail.com>
2019-02-23 23:41:40 +0330

    Some function added

--- accuracy.py
+++ accuracy.py
@@ -3,12 +3,30 @@ from sklearn.neighbors import KNeighborsClassifier as knn
 from sklearn.svm import NuSVC 
 from sklearn.svm import SVC 
 from sklearn.model_selection import cross_val_score as cv
-from sklearn.metrics import accuracy_score as ac_sc
+from sklearn.metrics import accuracy_score as acc
+from sklearn import tree 
 
 alpha = 0.01
 
+#Nearest neighbour for KNN
+nn = 10
+
+def cal_cost_tree(x, trn, trg):
+    x = [int(a) for a in np.round(x)]
+    x_index = [i for i in range(len(x)) if x[i]==1]
+    if sum(x) == 0 : return np.inf
+    trn = trn.reshape(trn.shape[1], -1)
+    trn = trn[x_index, :]
+    trn = np.transpose(trn)
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(trn, trg)
+    pre = clf.predict(trn)
+    score = acc(pre, trg)
+    error = 1 - score
+    return (1-alpha)*error + alpha * (sum(x)*1.0/len(x)), error, sum(x)*1.0/len(x)
+
+
 def cal_cost_knn(x, trn, trg):
-    nn = 10
     x = [int(a) for a in np.round(x)]
     x_index = [i for i in range(len(x)) if x[i]==1]
     if sum(x) == 0 : return np.inf
@@ -17,8 +35,8 @@ def cal_cost_knn(x, trn, trg):
     trn = np.transpose(trn)
     clf = knn(n_neighbors=nn)
     clf.fit(trn, trg)
-    score = cv(clf, trn, trg, cv=5, scoring="accuracy")
-    score = np.average(score)
+    pre = clf.predict(trn)
+    score = acc(pre, trg)
     error = 1 - score
     return (1-alpha)*error + alpha * (sum(x)*1.0/len(x)), error, sum(x)*1.0/len(x)
 
@@ -29,15 +47,15 @@ def cal_cost_svm(x, trn, trg):
     trn = trn.reshape(trn.shape[1], -1)
     trn = trn[x_index, :]
     trn = np.transpose(trn)
-    clf = SVC(gamma="auto")
+    clf = SVC(gamma="auto", kernel="rbf")
     clf.fit(trn, trg)
-    score = cv(clf, trn, trg, cv=5, scoring="accuracy")
-    score = np.average(score)
+    pre = clf.predict(trn)
+    score = acc(pre, trg)
     error = 1 - score
     return (1-alpha)*error + alpha * (sum(x)*1.0/len(x)), error, sum(x)*1.0/len(x)
 
 
-def test_acc(x, tst, tst_trg, trn, trn_trg):
+def test_acc_knn(x, tst, tst_trg, trn, trn_trg):
     x = [int(a) for a in np.round(x)]
     x = [i for i in range(len(x)) if x[i]==1]
     tst = tst.reshape(tst.shape[1], -1)
@@ -46,12 +64,39 @@ def test_acc(x, tst, tst_trg, trn, trn_trg):
     trn = trn.reshape(trn.shape[1], -1)
     trn = trn[x, :]
     trn = np.transpose(trn)
-    clf = knn(n_neighbors=5)
+    clf = knn(n_neighbors=nn)
     clf.fit(trn, trn_trg)
     tst_pred = clf.predict(tst)
-    acc = ac_sc(tst_trg, tst_pred)
-    print(acc)
-    return acc
+    return acc(tst_trg, tst_pred)
 
 
+def test_acc_svm(x, tst, tst_trg, trn, trn_trg):
+    x = [int(a) for a in np.round(x)]
+    x = [i for i in range(len(x)) if x[i]==1]
+    tst = tst.reshape(tst.shape[1], -1)
+    tst = tst[x, :]
+    tst = np.transpose(tst)
+    trn = trn.reshape(trn.shape[1], -1)
+    trn = trn[x, :]
+    trn = np.transpose(trn)
+    clf = SVC(gamma="auto", kernel="rbf")
+    clf.fit(trn, trn_trg)
+    tst_pred = clf.predict(tst)
+    return acc(tst_trg, tst_pred)
+    
+
+
+def test_acc_tree(x, tst, tst_trg, trn, trn_trg):
+    x = [int(a) for a in np.round(x)]
+    x = [i for i in range(len(x)) if x[i]==1]
+    tst = tst.reshape(tst.shape[1], -1)
+    tst = tst[x, :]
+    tst = np.transpose(tst)
+    trn = trn.reshape(trn.shape[1], -1)
+    trn = trn[x, :]
+    trn = np.transpose(trn)
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(trn, trn_trg)
+    tst_pred = clf.predict(tst)
+    return acc(tst_trg, tst_pred)
     
--- accuracy_test.py
+++ accuracy_test.py
@@ -1,35 +1,54 @@
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from accuracy import cal_cost_knn, cal_cost_svm
+from accuracy import cal_cost_knn, cal_cost_svm, cal_cost_tree
+from accuracy import test_acc_knn, test_acc_svm, test_acc_tree
+
 import numpy as np
 
 
 
-with open("dataset/waveform.data") as f:
-    lines = f.readlines()
-    data_x = []
-    data_y = []
-    for line in lines:
-        l = [float(d) for d in line.split(",")]
-        data_x.append(l[: -1])
-        data_y.append(l[-1])
+# with open("dataset/waveform.data") as f:
+#     lines = f.readlines()
+#     data_x = []
+#     data_y = []
+#     for line in lines:
+#         l = [float(d) for d in line.split(",")]
+#         data_x.append(l[: -1])
+#         data_y.append(l[-1])
         
-#dataset = datasets.load_breast_cancer()
-#data_x = dataset.data
-#data_y = dataset.target
+dataset = datasets.load_breast_cancer()
+data_x = dataset.data
+data_y = dataset.target
+# print(data_x)
+# print(data_y)
+
 data_x = np.array(data_x)
 data_y = np.array(data_y)
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.25)
 problem_dim = len(x_train[0])
 
-
-
-_, e, _= cal_cost_knn(np.ones(problem_dim), x_train, y_train) 
-e = 1 - e 
+dim = np.round(np.random.rand((problem_dim)))
+print(dim, sum(dim))
+#KNN
+_, e, _= cal_cost_knn(dim, x_train, y_train)
+e = 1 - e
 print("KNN train accuracy = {}".format(e))
+a = test_acc_knn(dim, x_test, y_test, x_train, y_train) 
+print("KNN test accuracy = {}".format(a))
 
-_, e, _= cal_cost_svm(np.ones(problem_dim), x_train, y_train)  
+#SVM
+_, e, _= cal_cost_svm(dim, x_train, y_train)
 e = 1 - e
 print("SVM(SVC) train accuracy = {}".format(e))
+a = test_acc_svm(dim, x_test, y_test, x_train, y_train) 
+print("SVM(SVC) test accuracy = {}".format(a))
+
+#TREE
+_, e, _= cal_cost_svm(dim, x_train, y_train)
+e = 1 - e
+print("Tree train accuracy = {}".format(e))
+a = test_acc_tree(dim, x_test, y_test, x_train, y_train) 
+print("Tree test accuracy = {}".format(a))
+
 
