76897b7ce246dc76cc8ab73762758164f17c7cb2
amir rassafi <amir.rassafi@gmail.com>
2019-02-26 14:14:49 +0330

    SVM kernel added as global parameter

--- accuracy.py
+++ accuracy.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier as knn
 from sklearn.svm import NuSVC 
 from sklearn.svm import SVC 
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import cross_val_score as cv
 from sklearn.metrics import accuracy_score as acc
 from sklearn import tree 
@@ -10,6 +11,7 @@ alpha = 0.01
 
 #Nearest neighbour for KNN
 nn = 10
+svm_kernel = "rbf"
 
 def cal_cost_tree(x, trn, trg):
     x = [int(a) for a in np.round(x)]
@@ -47,7 +49,7 @@ def cal_cost_svm(x, trn, trg):
     trn = trn.reshape(trn.shape[1], -1)
     trn = trn[x_index, :]
     trn = np.transpose(trn)
-    clf = SVC(gamma="auto", kernel="rbf")
+    clf = SVC(gamma="auto", kernel=svm_kernel)
     clf.fit(trn, trg)
     pre = clf.predict(trn)
     score = acc(pre, trg)
@@ -79,7 +81,7 @@ def test_acc_svm(x, tst, tst_trg, trn, trn_trg):
     trn = trn.reshape(trn.shape[1], -1)
     trn = trn[x, :]
     trn = np.transpose(trn)
-    clf = SVC(gamma="auto", kernel="rbf")
+    clf = SVC(gamma="auto", kernel=svm_kernel)
     clf.fit(trn, trn_trg)
     tst_pred = clf.predict(tst)
     return acc(tst_trg, tst_pred)
