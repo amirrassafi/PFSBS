6778c525cd883f8060e2978325eb2a19a5f8b749
amir <amir.rassafi@gmail.com>
2019-04-26 21:40:43 +0430

    some update strategy added

--- update_strategy.py
+++ update_strategy.py
@@ -25,14 +25,14 @@ UPDATE_STRATEGIES ={
             },
     "TCSSA4":
             {
-                "S1":(lambda t, T: 2.0*np.sqrt(2.5 - 2.5*np.tanh(t*6/T))),
+                "S1":(lambda t, T: 1.5*np.sqrt(2.5 - 2.5*np.tanh(t*6/T))),
                 "S2":(lambda t, T: 0.5 + 2.0*np.exp(-(3.0*t/T)**2)),
                 "S3":(lambda t, T: (-2.0 * t**3/T**3) + 2.5),
                 "S4":(lambda t, T: 1.5 * np.sqrt(2.5 - (2.0*np.log(t)/np.log(T)))) 
             },
     "TCSSA5":
             {
-                "S1":(lambda t, T: np.sqrt(-2.05 * t / T + 2.55)),
+                "S1":(lambda t, T: 1.5*np.sqrt(-2.05 * t / T + 2.55)),
                 "S2":(lambda t, T: 1.95 - 2.0*t**(1.0/3)/T**(1.0/3)),
                 "S3":(lambda t, T: -2.05 * t / T + 2.55),
                 "S4":(lambda t, T: (-2.0 * t**3/T**3) + 2.5)  
@@ -59,6 +59,18 @@ if __name__ == '__main__':
             f = UPDATE_STRATEGIES[st][sb]
             s = [f(t, T) for t in range(T)]
             ax = fig.add_subplot(3, 4, l)
+            l = l+1
+            ax.plot(s, c, label=name+"-"+sb)
+            ax.legend()
+    
+    for st, c, name in zip(["TCSSA1", "TCSSA4", "TCSSA5"], ['r', 'g', 'b'], ["M1", "M2", "M3"]):
+        fig = plt.figure()
+        l = 1
+        fig.suptitle(name)
+        for sb in sorted(UPDATE_STRATEGIES[st]):
+            f = UPDATE_STRATEGIES[st][sb]
+            s = [f(t, T) for t in range(T)]
+            ax = fig.add_subplot(2, 2, l)
             l = l+1
             ax.plot(s, c, label=name+"-"+sb)
             ax.legend()
