90b47396fe5908da90c6a068b6a3143a57d256d8
amir <amir.rassafi@gmail.com>
2019-04-26 21:40:27 +0430

    Addded for testing single process

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing 
 
 from bssa import BSSA
 from update_strategy import UPDATE_STRATEGIES as us
+from myplot import plot_cost_accuracy
 
+from accuracy import test_acc_svm
 import matplotlib.pyplot as plt
 import logging
+
+from dataset import load_dataset, replace_none_with_zero
+from dataset import load_mice, load_hepatitis, load_epileptic
+import time
+import pickle
 
 #Config logger
 logger = logging.getLogger("main")
@@ -22,29 +30,74 @@ logger.addHandler(ch)
 logger.addHandler(fh)
 
 if __name__ == '__main__':
-    #Load dataset
-    dataset = datasets.load_breast_cancer()
-    x_train, x_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size = 0.2, random_state=42)
-    #Set and define parameter
-    problem_dim = dataset.data.shape[1]
-    pop_size = 30
+
+    #parameters
+    iterations = 20
+    sync_iter = 5
+    pop_size = 20
     ub = 1
     lb = 0
-    tf = lambda x:1/(1+np.exp(-x))
-    strategy = "TCSSA1"
-    sub_chain = "S1"
+
+    logger = logging.getLogger("main.mp__main")
+
+    #Load dataset
+    #breast cancer dataset
+    dataset = datasets.load_breast_cancer()
+    dataset.data = preprocessing.scale(dataset.data)
+    x_train, x_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size = 0.2, random_state=42)
+
+    #hepatit
+    #data_x, data_y = load_hepatitis()
+    #data_x = replace_none_with_zero(data_x)
+    #data_x = preprocessing.scale(data_x)
+    #x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.2, random_state=42)
+
+    #Diabetes
+    #dataset = datasets.load_diabetes()
+    #dataset.data = preprocessing.scale(dataset.data)
+    #x_train, x_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size = 0.2, random_state=42)
+
+    #mice
+    #x, y = load_mice()
+    #x = preprocessing.scale(x)
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+
+    #epileptic
+    #x, y = load_epileptic()
+    #x = preprocessing.scale(x)
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+
+    #Set and define parameter
+    problem_dim = len(x_train[0])
+
+    def tf(x):
+        return 1/(1+np.exp(-x))
+    strategies = ["TCSSA2", "TCSSA2", "TCSSA2", "TCSSA2"]
+    sub_chains = ["S1", "S2", "S3", "S4"]
+
     #Define bssa object
-    logger.info("-" * 20 + "New Run" + "-" * 20+"pop_size = {} problem_dim = {} Method = {}".format(pop_size, problem_dim, strategy+" "+sub_chain))
-    bssa= BSSA(pop_size, problem_dim, tf, ub, lb, us[strategy][sub_chain])
-    cost, food, _ = bssa.train(50, x_train, y_train, x_test, y_test)
-    logger.info("best is {}, number of selected feature is {}".format(bssa.get_best_selected(), sum(bssa.get_best_selected())))
-    #Plot cost
-    cost_fig = plt.figure()
-    cost_ax = cost_fig.add_subplot(1, 1, 1)
-    cost_ax.plot(cost, label='cost')
-    cost_ax.legend()
-    food_fig = plt.figure()
-    food_ax = food_fig.add_subplot(1, 1, 1)
-    food_ax.plot(food, label = "num of selected feature")
-    food_ax.legend()
+    bssa_list = []
+    for st, su in zip(strategies, sub_chains):
+        bssa_list.append(BSSA(pop_size, problem_dim, tf, ub, lb, us[st][su]))
+
+
+    #Train
+    t = time.time()
+    for i in range(iterations//sync_iter):
+
+        #Create process
+        for bssa in bssa_list:
+            bssa.train(sync_iter, x_train, y_train, x_test, y_test)
+
+        bssa_list = sorted(bssa_list, key= lambda x: x.get_best_cost())
+        for bssa in bssa_list[1:]:
+            bssa.replace_with_worst_salp(bssa_list[0].get_best_salp())
+        logger.info("The best salp of each bssa replaced with worst one")
+
+    logger.info("Time = {}".format(time.time() - t))
+
+    for bssa, i in zip(bssa_list, range(len(bssa_list))):
+        plot_cost_accuracy(bssa, problem_dim)
+    sf = bssa_list[0].get_best_salp().get_position()
+    print("Test accuracy = {}".format(test_acc_svm(list(sf), x_test, y_test, x_train, y_train)))
     plt.show()
